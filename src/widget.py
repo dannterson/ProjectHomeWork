517a48827aa15aaacf5ba588609ee68c1fde9b7a
dannterson <dannterson@yandex.ru>
2025-03-05 03:32:47 +0300

    corrected import in widget

--- src/widget.py
+++ src/widget.py
@@ -1,5 +1,5 @@
-from masks import get_mask_card_number
-from masks import get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
+
 
 def mask_account_card(user_data: str) -> str:
     """Скрываем номер карты или счёта при помощи masks"""

e3ec8b8d34a74d10a2377be3ce953522593126a0
dannterson <dannterson@yandex.ru>
2025-03-24 01:29:33 +0300

    add test functions, pytest

---
+++ tests/test_masks.py
@@ -0,0 +1,7 @@
+def test_get_mask_card_number():
+    pass
+
+
+def test_get_mask_account():
+    pass
+
---
+++ tests/test_processing.py
@@ -0,0 +1,7 @@
+def test_filter_by_state():
+    pass
+
+
+def test_sort_by_date():
+    pass
+
